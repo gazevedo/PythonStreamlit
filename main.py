1fcce975b744597d4692dda16827c59ef34553fd
gabriel.azevedo <g05g12g82@>
2024-07-12 09:24:31 -0300

    alterando o main

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
+import os
+
 import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Meu site")
 
 
-@st.cache_data
-def carregar_dadps():
-    dados = pd.read_csv("resultados.csv")
-    return dados
+@st.cache
+def carregar_dados():
+    try:
+        dados = pd.read_csv("resultados.csv")
+        return dados
+    except FileNotFoundError:
+        st.error("Arquivo 'resultados.csv' não encontrado.")
+        return None
+    except pd.errors.EmptyDataError:
+        st.error("O arquivo 'resultados.csv' está vazio.")
+        return None
+    except pd.errors.ParserError:
+        st.error("Erro ao processar 'resultados.csv'. Verifique o formato dos dados.")
+        return None
 
 
 with st.container():
-    st.title("Dashbord Contratos")
+    st.title("Dashboard Contratos")
     st.write("Hello **world**!")
     st.write("Mês de Janeiro")
     st.write("[Link](https://www.google.com.br/)")
     st.write("---")
 
 with st.container():
-    qtd_dias = st.selectbox("Slecione o período", ["1D", "15D", "31D"])
+    qtd_dias = st.selectbox("Selecione o período", ["1D", "15D", "31D"])
     num_dias = int(qtd_dias.replace("D", ""))
-    dados = carregar_dadps()
-    dados = dados[-num_dias:]
-    st.area_chart(dados, x="Data", y="Contratos")
+
+    dados = carregar_dados()
+    if dados is not None:
+        dados = dados[-num_dias:]
+        st.area_chart(dados, x="Data", y="Contratos")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    st.set_option('server.port', port)
+    st.set_page_config(layout="wide")
+    st.title('Meu aplicativo Streamlit')
+    st.write('Hello world!')
