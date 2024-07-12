17a59514c6a30ccc297ebd0205f4b560fc30130c
gabriel.azevedo <g05g12g82@>
2024-07-12 11:30:42 -0300

    teste

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import streamlit as st
 import pandas as pd
 
 # Configuração da página do Streamlit
-st.set_page_config(page_title="Meu Aplicativo Streamlit", layout="wide")
+st.set_page_config(page_title="Meu Aplicativo", layout="wide")
 
 # Função para carregar dados (exemplo)
-@st.cache
 def carregar_dados():
     dados = pd.read_csv("dados.csv")
     return dados
 
 # Corpo principal do aplicativo Streamlit
-st.title('Meu Aplicativo Streamlit')
+st.title('Meu Aplicativo')
 st.write('Hello world!')
 
 # Exemplo de uso de dados carregados
